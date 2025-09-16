36e5d03e81a57aea378aac9e2f59539e6946eebf
kallekaa <kalle.kaarlela@gmail.com>
2025-09-16 21:36:29 +0300

    streamlit app working

--- backend/core.py
+++ backend/core.py
@@ -1,36 +1,82 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv() # dotenv_path="../.env"
+load_dotenv()
+from typing import Any, Dict, List
 
 from langchain import hub
 from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.history_aware_retriever import \
+    create_history_aware_retriever
 from langchain.chains.retrieval import create_retrieval_chain
-from langchain_pinecone import PineconeVectorStore
-
+from langchain_chroma import Chroma
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
-def run_llm(query: str):
+from langchain_pinecone import PineconeVectorStore
+
+#from consts import INDEX_NAME
+
+embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+#chroma = Chroma(persist_directory="chroma_db", embedding_function=embeddings)
+
+
+def run_llm(query: str, chat_history: List[Dict[str, Any]] = []):
     embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+    #docsearch = Chroma(persist_directory="chroma_db", embedding_function=embeddings)
     docsearch = PineconeVectorStore(index_name=os.getenv("PINECONE_INDEX"), embedding=embeddings)
-    chat = ChatOpenAI(model="gpt-4o-mini", verbose=True, temperature=0)  #api_key=os.getenv("OPENAI_API_KEY")
+    chat = ChatOpenAI(verbose=True, temperature=0)
 
-    retrieval_qa_chat_promt = hub.pull("langchain-ai/retrieval-qa-chat")
-    stuff_documents_chain = create_stuff_documents_chain(chat, retrieval_qa_chat_promt)
+    rephrase_prompt = hub.pull("langchain-ai/chat-langchain-rephrase")
 
-    qa = create_retrieval_chain(retriever=docsearch.as_retriever(), combine_docs_chain=stuff_documents_chain)
+    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+    stuff_documents_chain = create_stuff_documents_chain(chat, retrieval_qa_chat_prompt)
 
-    result = qa.invoke(input={"input":query})
-    new_result = {
-        "query": result["input"],
-        "result": result["answer"],
-        "source_documents": result["context"],
-    }
+    history_aware_retriever = create_history_aware_retriever(
+        llm=chat, retriever=docsearch.as_retriever(), prompt=rephrase_prompt
+    )
+    qa = create_retrieval_chain(
+        retriever=history_aware_retriever, combine_docs_chain=stuff_documents_chain
+    )
 
-    return new_result
+    result = qa.invoke(input={"input": query, "chat_history": chat_history})
+    return result
+
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+
+def run_llm2(query: str, chat_history: List[Dict[str, Any]] = []):
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+    #docsearch = Chroma(persist_directory="chroma_db", embedding_function=embeddings)
+    docsearch = PineconeVectorStore(index_name=os.getenv("PINECONE_INDEX"), embedding=embeddings)
+    chat = ChatOpenAI(model="gpt-4o-mini", verbose=True, temperature=0)
+
+    #rephrase_prompt = hub.pull("langchain-ai/chat-langchain-rephrase")
+
+    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+
+    rag_chain = (
+        {
+            "context": docsearch.as_retriever() | format_docs,
+            "input": RunnablePassthrough(),
+        }
+        | retrieval_qa_chat_prompt
+        | chat
+        | StrOutputParser()
+    )
+
+    retrieve_docs_chain = (lambda x: x["input"]) | docsearch.as_retriever()
+
+    chain = RunnablePassthrough.assign(context=retrieve_docs_chain).assign(
+        answer=rag_chain
+    )
+
+    result = chain.invoke({"input": query, "chat_history": chat_history})
+    return result
 
 if __name__ == "__main__":
-    res = run_llm(query="What is a LangChain Chain?")
-    print(res["result"])
-
-
+    res = run_llm(query="What is LangChain?")
+    print(res["answer"])
